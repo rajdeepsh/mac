d17ac7020734e92fdc5e816c48c159d961ef1793
Rajdeep Singh Hundal <rajd33psh@gmail.com>
2026-03-20 17:35:38 +0800

    fin

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mac
+
+Execute the following command to set up your new Mac:
+
+> [!IMPORTANT]
+> [install.sh](install.sh) assumes that you have copied your `.ssh` folder into `$HOME` and that it contains three files; `config`, `rajdeepsh`, and `rajdeepsh.pub`.
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/rajdeepsh/mac/main/install.sh | bash
+```
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if ! command -v brew >/dev/null 2>&1; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> $HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+fi
+
+echo "Setting up your mac..."
+curl -fsSL https://raw.githubusercontent.com/rajdeepsh/mac/main/brewfile.txt | brew bundle --file=-
+
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 1
+
+mkdir -p $HOME/Repos
+
+chmod 700 $HOME/.ssh
+chmod 644 $HOME/.ssh/rajdeepsh.pub
+chmod 600 $HOME/.ssh/rajdeepsh $HOME/.ssh/config
+
+git config --global user.name "Rajdeep Singh Hundal"
+git config --global user.email "rajd33psh@gmail.com"
+git config --global gpg.format ssh
+git config --global user.signingkey $HOME/.ssh/rajdeepsh.pub
+git config --global commit.gpgsign true
+
+git -C $HOME clone git@github.com:rajdeepsh/mac.git
+
+echo
+gum spin --spinner globe --title "Done! Press any key to reboot..." -- bash -c 'read -n 1 -s'
+sudo shutdown -r now
